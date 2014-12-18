18cef400806fb868f1321dda149d9811c99fd1ed
Siying Zhang <siying_zh@yahoo.com>
2014-12-18 16:20:14 -0600

    Update two_sum.h

--- two_sum.h
+++ two_sum.h
@@ -2,11 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         std::vector<int> result;
-        std::map<int,int> numTable;
+        std::map<int,int> numTable;  
+        // create a hash table maps numbers to indices
         for(std::vector<int>::size_type i=0;i<numbers.size();i++){
             numTable[numbers.at(i)] = i+1;
         }
         for(std::vector<int>::size_type i=0;i<numbers.size();i++){
+            // 
             std::map<int,int>::const_iterator it = numTable.find(target - numbers.at(i));
             if(it->second==i+1)continue;
             if(it!=numTable.end()){
