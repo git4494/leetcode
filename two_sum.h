7aa0535ca3e2a39060aa53658aa5f63647c2ab8b
Siying Zhang <siying_zh@yahoo.com>
2014-12-18 16:06:20 -0600

    hash table

---
+++ two_sum.h
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        std::vector<int> result;
+        std::map<int,int> numTable;
+        for(std::vector<int>::size_type i=0;i<numbers.size();i++){
+            numTable[numbers.at(i)] = i+1;
+        }
+        for(std::vector<int>::size_type i=0;i<numbers.size();i++){
+            std::map<int,int>::const_iterator it = numTable.find(target - numbers.at(i));
+            if(it->second==i+1)continue;
+            if(it!=numTable.end()){
+                if(it->second<i+1){
+                result.push_back(it->second);
+                result.push_back(i+1);
+                }
+                else{
+                result.push_back(i+1);
+                result.push_back(it->second);
+                }
+                break;
+            }
+        }
+        return result;
+      
+    }
+};
